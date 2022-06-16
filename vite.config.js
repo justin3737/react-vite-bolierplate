0365cd7b91c77c20ffa440c4bd1ab5c11c585999
Justin Wu <juns3700@gmail.com>
2022-06-16 18:32:36 +0800

    adjust bundle method

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+const { resolve } = require('path');
 import react from '@vitejs/plugin-react'
 import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
@@ -7,11 +8,15 @@ export default defineConfig({
   plugins: [
     react(),
     chunkSplitPlugin({
-      strategy: 'single-vendor'
+      strategy: 'default'
     })
   ],
   build: {
     rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        //arcticle: resolve(__dirname, 'arcticle/index.html')
+      },
       output: {
         entryFileNames: `assets/[name].js`,
         chunkFileNames: `assets/[name].js`,
