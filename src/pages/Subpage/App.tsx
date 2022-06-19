5b824ba937de67435561d70cb0025dd45df53767
Justin Wu <juns3700@gmail.com>
2022-06-19 23:11:05 +0800

    remove eslint

--- src/pages/Subpage/App.tsx
+++ src/pages/Subpage/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import useAxios from '/src/utils/useAxios';
+import React, { useState, useEffect } from "react"
+import "./App.css"
+import useAxios from "/src/utils/useAxios"
 
 
 function App() {
